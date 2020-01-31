985890629ed1df58690c434a25ce230a8a42d201
JayaKrishna <namburu1995@gmail.com>
2020-01-31 20:46:42 +0530

    One more trail

--- utils/helper.ts
+++ utils/helper.ts
@@ -17,31 +17,10 @@ export const getMonth = givenMonth => {
 };
 
 export const requestNotifications = () => {
-  // Let's check if the browser supports notifications
   if (!("Notification" in window)) {
     alert("This browser does not support desktop notification");
-  }
-
-  // Let's check whether notification permissions have already been granted
-  else if (Notification.permission === "granted") {
-    sendNotification("Welcome to JSRealm");
-  }
-
-  // Otherwise, we need to ask the user for permission
-  else if (Notification.permission !== "denied") {
-    try {
-      Notification.requestPermission().then(() =>
-        sendNotification("Welcome to JSRealm")
-      );
-    } catch (error) {
-      if (error instanceof TypeError) {
-        Notification.requestPermission(() => {
-          sendNotification("Welcome to JSRelam");
-        });
-      } else {
-        throw error;
-      }
-    }
+  } else if (Notification.permission !== "denied") {
+    Notification.requestPermission();
   }
 };
 
