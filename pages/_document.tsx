e9a42f44b9dedfcc035867e0555fe4c0ed2c4df6
JayaKrishna <namburu1995@gmail.com>
2020-01-31 21:35:26 +0530

    Added meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,10 +5,22 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link rel="icon" href="../images/favicon.ico" />
           <link
             href="https://fonts.googleapis.com/css?family=Lato&display=swap"
             rel="stylesheet"
           />
+          <meta
+            name="description"
+            content="A live Q&A tool built for making a remote meetup experience interactive"
+          />
+          <meta
+            name="twitter:card"
+            value="A live Q&A tool built for making a remote meetup experience interactive"
+          ></meta>
+          <meta property="og:title" content="JSRealm live Q&A" />
+          <meta property="og:type" content="chat" />
+          <meta property="og:url" content="https://www.jsrealm.in/" />
         </Head>
         <body>
           <Main />
